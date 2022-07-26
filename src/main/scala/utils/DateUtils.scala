beda6886eb5ae27638597161362732851e54d83e
Michele Scarpenti <mscarpenti@cuebiq.com>
2022-07-27 01:25:31 +0200

    cleaned DateUtils

--- src/main/scala/utils/DateUtils.scala
+++ src/main/scala/utils/DateUtils.scala
@@ -1,15 +1,9 @@
 package it.scarpenti.marioinc
 package utils
 
-import java.text.SimpleDateFormat
 import java.time.{Instant, LocalDate}
-import java.util.TimeZone
 
 object DateUtils {
-
-  val dashedFormatter = new SimpleDateFormat("yyy-MM-dd")
-  val tsUtcFormatter = new SimpleDateFormat("yyy-MM-dd'T'HH:mm:ss.SSSX")
-  tsUtcFormatter.setTimeZone(TimeZone.getTimeZone("GMT"))
 
   def toLocalDate(dateStr: String): LocalDate = {
     LocalDate.parse(dateStr)
