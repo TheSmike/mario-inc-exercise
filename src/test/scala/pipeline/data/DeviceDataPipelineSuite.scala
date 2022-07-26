b9224a00e3e8d2616ccaee645a2b19145fc6eb24
Michele Scarpenti <mscarpenti@cuebiq.com>
2022-07-26 23:52:03 +0200

    unified tests to avoid session problems

--- src/test/scala/pipeline/data/DeviceDataPipelineSuite.scala
+++ src/test/scala/pipeline/data/DeviceDataPipelineSuite.scala
@@ -1,10 +1,13 @@
 package it.scarpenti.marioinc
 package pipeline.data
 
-import model.RawDevice
+import model.{Device, RawDevice}
 import spark.SparkSessionFactory
 import utils.DateUtils.toLocalDate
 
+import io.delta.tables.DeltaTable
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.types.{DateType, LongType, StringType, TimestampType}
 import org.scalatest.BeforeAndAfterAll
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.must.Matchers._
@@ -13,12 +16,20 @@ import org.scalatest.matchers.should.Matchers.convertToAnyShouldWrapper
 
 class DeviceDataPipelineSuite extends AnyFunSuite with BeforeAndAfterAll {
 
+  final val database = "tests_db"
+  final val outputTableName = s"$database.test_duplicates_table"
+
   private val session = SparkSessionFactory.getSession("local")
   private val sc = session.sparkContext
   private val logic = new DeviceDataLogic(session, 1)
 
   import session.implicits._
 
+
+  override def beforeAll(): Unit = {
+    session.sql(s"CREATE DATABASE IF NOT EXISTS $database LOCATION '/tmp/marioinc/tests_db/' ")
+    //FIXME in test env this command logs (but don't throws) the exception AlreadyExistsException
+  }
 
   test("filterRawData should remove records older then 1 day") {
     val receivedDate = "2021-01-03"
@@ -63,6 +74,42 @@ class DeviceDataPipelineSuite extends AnyFunSuite with BeforeAndAfterAll {
 
     result should have size 4
     result should contain allOf((id1, ts1), (id2, ts2), (id3, ts3), (id3, ts4))
+  }
+
+  test("multiple pipeline runs shouldn't load duplicated records") {
+    val rawInputDs = session.read.json("src/test/resources/test_data/duplicates_on_different_dates")
+    val outputDeltaTable = createTmpDataDeltaTable(session, outputTableName)
+
+    logic.run(toLocalDate("2021-04-01"), rawInputDs, outputDeltaTable)
+    logic.run(toLocalDate("2021-04-02"), rawInputDs, outputDeltaTable)
+
+    val result = session.read.table(outputTableName).as[Device].collect.toList
+      .map(r => (r.device, r.event_timestamp))
+
+    result should contain theSameElementsAs result.toSet
+  }
+
+  private def createTmpDataDeltaTable(session: SparkSession, tableName: String): DeltaTable = {
+    if (DeltaTable.isDeltaTable(tableName))
+      DeltaTable.forName(tableName).delete()
+
+    DeltaTable.createOrReplace(session)
+      .tableName(tableName)
+      .addColumn(Device.RECEIVED_DATE, DateType)
+      .addColumn(Device.EVENT_TIMESTAMP, TimestampType)
+      .addColumn(Device.DEVICE, StringType)
+      .addColumn(Device.CO2_LEVEL, LongType)
+      .addColumn(Device.HUMIDITY, LongType)
+      .addColumn(Device.TEMPERATURE, LongType)
+      .addColumn(
+        DeltaTable.columnBuilder(Device.EVENT_DATE)
+          .dataType(DateType)
+          .generatedAlwaysAs(s"CAST(${Device.EVENT_TIMESTAMP} AS DATE)")
+          .build())
+      .partitionedBy(Device.EVENT_DATE, Device.DEVICE)
+      .location(s"/tmp/marioinc/${tableName.replace('.', '/')}/")
+      .execute()
+    //TODO part of this method could be refactored to match the create table in the init pipeline
   }
 
 }
